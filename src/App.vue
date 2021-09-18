2025212356594951edf1d050f1e81d3c4620bc04
Atul Kumar Singh <atul.kumar@codelogicx.com>
2021-09-18 11:23:12 +0530

    simple quotable app

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="app">
+  <Header title="Quotable App"/>
+  <Quote :quote="quote"/>
+  <div class="button-container">
+			<button @click="getQuotes">GET A NEW QUOTE</button>
+		</div>
+<QuoteList :quotes="quotes"/>
+
+</div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Quote from './components/Quote'
+import QuoteList from './components/QuotesList'
+export default {
+  name: 'App',
+  components: {Header,Quote,QuoteList},
+  data(){
+    return{
+      quote :{},
+      quotes : []
+  }
+},
+methods:{
+  async getQuotes(){
+     if(this.quote.content){
+       this.quotes = [...this.quotes,this.quote];
+     }
+     const data = await fetch('https://api.quotable.io/random').then(res => res.json()); 
+    this.quote = {
+        content: data.content,
+        author : data.author,
+        tags   : data.tags[0]
+     };
+ }
+},
+created(){
+  this.getQuotes();
+}
+}
+</script>
+
+<style lang="scss">
+:root {
+	--primary: #D81E5B;
+	--secondary: #8A4FFF;
+	--tertiary: #32CBFF;
+	--dark: #131A26;
+	--light: #EEE;
+	--grey: #848484;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira Sans', sans-serif;
+}
+
+.button-container {
+	display: flex;
+	justify-content: center;
+	padding: 0px 32px;
+	margin: 64px auto;
+
+	button {
+		border: none;
+		outline: none;
+		background-color: var(--primary);
+		padding: 16px 32px;
+		border-radius: 99px;
+
+		color: var(--light);
+		font-size: 1.5em;
+		font-weight: 700;
+		text-transform: uppercase;
+		cursor: pointer;
+		transition: 0.4s;
+
+		&:hover {
+			background-color: var(--secondary);
+		}
+	}
+}
+</style>
